9c16cc59d3f1a8fa93452dae9ab1cfe7e04c53af
Tiago <tjoliveirafpereira@hotmail.com>
2019-01-27 10:55:17 +0000

    Resource Simulation

---
+++ Assets/Project/Scripts/Sectors/FuelProductionSector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelProductionSector : Sector
+{
+
+    ResourceManager resourceManagerRef;
+    [SerializeField] float RequiredWater = 5;
+    [SerializeField] float defaultFuelProduction = 0.3f;
+    private void Start()
+    {
+        base.Start();
+        resourceManagerRef = GameObject.FindObjectOfType<ResourceManager>();
+    }
+
+
+    public float CalculateResourceProduced()
+    {
+        if (resourceManagerRef.checkWaterQuantity() && !(ResourceManager.getInstance().getFuelPercent() >= 100))
+        {
+            resourceManagerRef.DecrementWater(RequiredWater);
+            float EngDecay = (getEngineerCount() / maximumCapacity) * 2;
+            float EngPower = Mathf.Clamp(EngDecay, SectorManager.getInstance().getMinimumResourceProduction(), SectorManager.getInstance().getMaximumResourceProduction());
+
+            return RequiredWater * EngPower;
+        }
+        return 0.0f;
+    }
+}
---
+++ Assets/Project/Scripts/UI/Sectors/GetSectorHappy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetSectorHappy : MonoBehaviour {
+
+
+    Text textRef;
+    Sector sectorRef;
+
+    // Use this for initialization
+    void Start()
+    {
+        textRef = GetComponent<Text>();
+        sectorRef = GetComponentInParent<Sector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textRef.text = (sectorRef.GetHappyLevelInSector() * 100).ToString();
+
+    }
+}
